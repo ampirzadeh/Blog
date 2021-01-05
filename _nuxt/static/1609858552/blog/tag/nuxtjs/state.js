5b8c58673caca9dafc298d42dc616d920c466cad
ampirzadeh <ampirzadeh@users.noreply.github.com>
2021-01-05 14:55:53 +0000

    deploy: 8fd7ef04f7a4c01c06fcf60437d4ac4b160ba32f

---
+++ _nuxt/static/1609858552/blog/tag/nuxtjs/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1609858552",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{content:{dbHash:"55324e1c"}}}}("\u002Fblog\u002Ftag\u002Fnuxtjs"));
